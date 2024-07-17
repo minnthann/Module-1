a6e1a9764250b48563a3711f15318ebc30ffd9ee
minnthann <panyobhasa93@gmail.com>
2024-07-17 14:26:44 +0630

    javascript 1

--- exercised/javascript/laven2/assets/js/script.js
+++ exercised/javascript/laven2/assets/js/script.js
@@ -1,115 +1,4 @@
 
-// var x = 10;
-
-// function y () {
-//     x ++;
-//     // alert (y);
-//     document.querySelector("h1").innerHTML = x;
-
-//     if (x % 3 === 0) {
-//         alert('You got it right, Now the number is ${x}');
-//     }
-// }
-
-/*
-// 2
-var x = man;
-
-function y () {
-    x = x + "1";
-    // alert (y);
-    document.querySelector("h1").innerHTML = x;
-
-    if (x % 3 === 0) {
-        alert('You got it right, Now the number is ${x}');
-    }
-}
-    */
-/*
-auto ဆတိုး
-var x = 0;
-
-function y () {
-    x = x + 1;
-    document.querySelector("h1").innerHTML = x;
-
-    document.querySelector('button').onclick = y;
-
-setInterval(y, 1000);  //1 -0
-}
-setInterval(y, 1000);  //1-1
-*/
-
-/*
-// var x = 0;
-// စက္ကန့်ရပ်
-
-if (!localStorage.getItem('mm')) {
-    localStorage.setItem('mm', 0);
-}
-function y () {
-    let mm = localStorage.getItem('x');
-        mm ++;
-
-        document.querySelector("h1").innerHTML = mm;
-        localStorage.setItem('x', mm);
-}
-
-document.querySelector('h1').innerHTML = localStorage.getItem('x');
-document.querySelector('button').onclick = q;    
-
-// တူ
-// let x = 0 ;
-
-if (!localStorage.getItem('key')) {
-    localStorage.setItem ('key', 0);
-}
-
-
-function y () {
-    let update = localStorage.getItem('key');
-   update ++;
-    document.querySelector('h1').innerHTML = update;
-    localStorage.setItem('key', update);
-}
-document.querySelector('h1').innerHTML = localStorage.getItem('key');
-document.querySelector('button').onclick = y;
-
-
-// localStorage
-// localStorage,getItem('key')
-// localStorage.setItem('key', value)
-*/
-
-
-
-function ygn () {
-    if(document.querySelector('h1').innerHTML === "hello, ygn") {
-        document.querySelector ('h1').innerHTML = "မင်္ဂလာပါ"
-    }else {
-        document.querySelector('h1').innerHTML = "hello ygn";
-    }
-    
-}
-
-// ဖောင့်မှာ နာမည်ဖြည့်
-
-
-document.querySelector('#submit').disabled = true;
-
-document.querySelector('#name').onkeyup = function() {
-    document.querySelector('#submit').disabled = false;
-}
-
-document.querySelector('form').onsubmit = function() {
-    let urName = document.querySelector('#name').value;
-    alert('မင်္ဂလာပ, ${urName}');
-}
-
-
-
-// ပီး
-/*
 // li .ကို ဖျေက်ထားနည်း
 document.querySelector('#submit').disabled = true;
 
@@ -121,7 +10,7 @@ document.querySelector('#task').onkeyup = function () {
         document.querySelector('#submit').disabled = true;
     }
 }
-
+/*
     //8. အပေါ်က သုံးမယ်ဆို မလိုပါ
     // onkeyup ရေးသားနည်း
     document.querySelector('#task').onkeyup = function () {
@@ -130,12 +19,7 @@ document.querySelector('#task').onkeyup = function () {
 
 }
 */ 
-// function အစားထိုးခြင်း
-// = () => {
 
-// }
-
-/*
 // ၇.
 document.querySelector("form").onsubmit = function () {
     //၂. ရေးထားပီးသောစားကို သိမ်း
@@ -162,4 +46,9 @@ document.querySelector("form").onsubmit = function () {
     // submit-နိပ်လိုက်သောအခါ page refresh မလုပ်ရန်
     return false;
 }
-*/
+
+
+// function အစားထိုးခြင်း အသုံးပြုပုံ
+
+// function ( ) { }
+// = () => { }
