51819908af139b85cc52a01170f5fbdf085b049f
minnthann <panyobhasa93@gmail.com>
2024-08-19 09:22:39 +0630

    02challage

---
+++ sa.js
@@ -0,0 +1,55 @@
+// နိုပ်ရမဲ့ ဘက်တန်
+var generateBtn = document.querySelector (
+    "#generate");
+
+generateBtn.addEventListener ("click",writePassword);
+
+function writePassword () {
+    //ရလာသော ပစည်းကို သိမ်းပါ
+    var password = generatedPassword();
+    // အလယ်မှာ ရေးပါ
+    var passwordText = document.querySelector("#password");
+    // value သိမ်းပေါ်
+    passwordText.value = password;
+}
+
+function generatedPassword(){
+    let passwordLength;
+    let includeUpperCase;
+    let includeLowerCase;
+    let includeNumber;
+    let includeSpecialCharacter;
+
+
+    passwordLength = parseInt(prompt("စကားဝှာက်ကို ၈-လုံးမှ ၁၂၈-လုံးအတွင်းရွေးပါ"));
+    includeUpperCase = confirm('စာလုံးကြီးရောပါစေချင်သေးလား');
+    includeLowerCase = confirm('စာလုံးသေးရောထည်ချင်သေးလား');
+    includeNumber = confirm ('နံပါတ်ရောထည့်ချင်သေးလား');
+    includeSpecialCharacter = confirm ('အထူးစကားလုံးတွေရောထည့်ချင်သေးလား');
+//မမှန်မချင်း ပြန်မေးပါ  မှန်လျှင် ရှေ့ဆက်ပါ while
+    while (isNaN(passwordLength) || passwordLength <8 || passwordLength >128){
+        alert('ဒါလေးတောင်မှန်အော်မထည့်တတ်ဘူးလား၊ ၈လုံးမှ ၁၂၈ လုံးအတွင်းရွေးပါဆိုမှ');
+        return "";
+    }
+//ထပ်ခါထပ်ခါ ပြန်မေးပါ
+    if (!includeUpperCase && !includeLowerCase && includeNumber && includeSpecialCharacter) {
+        alert ('ဟဲ့ကောင်စုတ်၊ တစ်ခုခုရွှေးပါဆိုမှ') 
+        return "";
+    }
+    let availableChar = "";
+    if (includeUpperCase) {
+        availableChar += 'ABCDEF';
+    }if (includeLowerCase){
+        availableChar += 'abcdef';
+    }
+    if (includeNumber){
+        availableChar += '12345';
+    }(includeSpecialCharacter){
+        availableChar += 'ဣဤ၍ဪ';
+    }
+    let password = "";
+    for (let i=0; i<passwordLength; i++){
+        let rendomIndex  = Math.floor(Math.random()* availableChar.length); password +=  availableChar.charAt(rendomIndex);
+    }
+    return password;
+}
